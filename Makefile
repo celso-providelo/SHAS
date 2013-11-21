5367e5828f3ff15cb256bacee77eb2ebb6e6b28a
Celso Providelo <celso.providelo@gmail.com>
2013-11-21 03:08:10 -0200

    Including some home-automation related elements.

--- Makefile
+++ Makefile
@@ -11,6 +11,10 @@ run-gunicorn:
 	@echo "Running Flask app via Gunicorn"
 	gunicorn -k flask_sockets.worker shas:app -t 3
 
+run-public:
+	@echo "Running Flask app via Gunicorn publicly"
+	gunicorn -k flask_sockets.worker shas:app -t 3 -b 0.0.0.0
+
 run:
 	@echo "Running Flask app via Gevent"
 	$(PYTHON) shas.py
--- shas.py
+++ shas.py
@@ -4,13 +4,35 @@
 
 """
 from __future__ import unicode_literals
+import datetime
 
 from flask import (
     json, render_template, Flask)
 from flask_sockets import Sockets
+from gevent import spawn, spawn_later
+
 
 app = Flask(__name__)
 sockets = Sockets(app)
+app.debug = True
+
+
+def send_event(ws, status):
+    data = {
+        'label': 'Movimento Sala TV',
+        'action': status,
+        'timestamp': datetime.datetime.now().isoformat(),
+    }
+    message = json.dumps(data)
+    ws.send(message)
+
+    if status == 'ON':
+        status = 'OFF'
+    else:
+        status = 'ON'
+
+    spawn_later(20, send_event, ws, status)
+
 
 @sockets.route('/bus')
 def bus(ws):
@@ -18,8 +40,13 @@ def bus(ws):
         message = ws.receive()
         if not message:
             continue
+
         data = json.loads(message)
-        data['text'] += '-XXX'
+        if data.get('action') == 'start':
+            spawn(send_event, ws, 'ON')
+            continue
+
+        data['timestamp'] = datetime.datetime.now().isoformat()
         message = json.dumps(data)
         ws.send(message)
 
@@ -33,7 +60,6 @@ if __name__ == '__main__':
     #app.run(debug=True)
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-    app.debug = True
     server = pywsgi.WSGIServer(
         ('', 8000), app, handler_class=WebSocketHandler)
     server.serve_forever()
